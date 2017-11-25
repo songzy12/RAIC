abf8ba751d4ba8b29416ecedaeb1bc543edcb719
unknown <songzy.thu@qq.com>
2017-11-25 21:36:13 +0800

    permutation

--- java-cgdk/src/main/java/MyStrategy.java
+++ java-cgdk/src/main/java/MyStrategy.java
@@ -80,7 +80,7 @@ public final class MyStrategy implements Strategy {
                 continue;
             if (v.getType() != type) 
                 continue;
-            System.out.println(v.getType() + " " + v.getX() + " " + v.getY());
+            //System.out.println(v.getType() + " " + v.getX() + " " + v.getY());
             if (v.getX() <= x && v.getY() <= y) {
                 x = v.getX();
                 y = v.getY();
@@ -90,11 +90,7 @@ public final class MyStrategy implements Strategy {
         return v_;
     }
     
-    private double[][] positions = new double[5][2];      
-    
     private void move_to(VehicleType v, double x, double y, int tick) {
-        
-        
         if (tick == 0) {
             move.setAction(ActionType.CLEAR_AND_SELECT);
             move.setVehicleType(v);
@@ -104,7 +100,6 @@ public final class MyStrategy implements Strategy {
         
         if (tick == 1) {
             System.out.println("move_to, " + x + " " + y);
-        
             move.setAction(ActionType.MOVE);
             move.setX(x);
             move.setY(y);
@@ -124,64 +119,84 @@ public final class MyStrategy implements Strategy {
         if (tick == 1) {            
             move.setAction(ActionType.SCALE);
             Vehicle v_ = find_vehicle(me, v);
-            double delta = game.getVehicleRadius();
+            double delta = 10 * game.getVehicleRadius();
             double [] origin = {v_.getX(), v_.getY()};
-            move.setX(origin[0]);
-            move.setY(origin[1]);
+            move.setX(origin[0] + delta);
+            move.setY(origin[1] + delta);
             move.setFactor(factor);
             move.setMaxSpeed(game.getTankSpeed());
         }
     }
     
     private void line_up(int tick) {
-        double f = 2;
-        if (tick < 2)
-            scale(types[1], tick, f);
         
-        // move further
-        /*if (tick < 10) {
-            for (int i = 0; i < 5; ++i) {
-                double delta = 30 * game.getVehicleRadius() + 2 * i * game.getVehicleRadius();
-                
-                move_to(types[i], delta, delta, tick - i * 2);
+        double left = 18, middle = 92, right = 166;
+        double delta = game.getVehicleRadius();
+        
+        // permutate
+        if (tick < 1000) {
+            if (tick < 2 && positions0[0].x != left) {
+                move_to(positions0[0].type, left - positions0[0].x, 0, tick);
+            } else if (tick >= 2 && tick < 4 && positions0[1].x != middle) {
+                move_to(positions0[1].type, middle - positions0[1].x, 0, tick - 2);
+            } else if (tick >= 4 && tick < 6 && positions0[2].x != right) {
+                move_to(positions0[2].type, right - positions0[2].x, 0, tick - 4);
+            } else if (tick >= 500 && tick < 502 && positions0[0].y != middle + 2 * delta) {
+                move_to(positions0[0].type, 0, middle + 2 * delta - positions0[0].y, tick - 500);
+            } else if (tick >= 502 && tick < 504 && positions0[1].y != middle) {
+                move_to(positions0[1].type, 0, middle - positions0[1].y, tick - 502);
+            } else if (tick >= 504 && tick < 506 && positions0[2].y != middle - 2 * delta) {
+                move_to(positions0[2].type, 0, middle - 2 * delta - positions0[2].y, tick - 504);
             }
-        }*/
+        }
         
+        if (tick < 1000) {
+            if (tick >= 6 && tick < 8 && positions1[0].x != left) {
+                move_to(positions1[0].type, middle - positions1[0].x, 0, tick - 6);
+            } else if (tick >= 8 && tick < 10 && positions1[1].x != right) {
+                move_to(positions1[1].type, right - positions1[1].x, 0, tick - 8);
+            } else if (tick >= 506 && tick < 508 && positions1[0].y != middle) {
+                move_to(positions1[0].type, 0, middle - positions1[0].y, tick - 506);
+            } else if (tick >= 508 && tick < 510 && positions1[1].y != middle + 2*delta) {
+                move_to(positions1[1].type, 0, middle+2*delta - positions1[1].y, tick - 508);
+            }
+        }
+        
+        
+        /*
         // scale 
-        if (tick >= 500 && tick < 510) {
+        if (tick >= 200 && tick < 210) {
             double factor = 3;
             for (int i = 0; i < 5; ++i)
-                scale(types[i], tick - 500 - i * 2, factor);
+                scale(types[i], tick - 200 - i * 2, factor);
             return;
         }
         
         // intersect
-        if (tick >= 1000 && tick < 1010) {
-            double mid_x = positions[2][0] + 30 * game.getVehicleRadius();        
+        if (tick >= 500 && tick < 510) {
+            double mid_x = 0;
             for (int i = 0; i < 5; ++i) {
                 Vehicle v_ =  find_vehicle(me, types[i]);
-                double delta = game.getVehicleRadius();
                 if (Math.abs(v_.getX() - mid_x) > delta)
-                    move_to(types[i], mid_x + 2 * i * delta - v_.getX() , 0, tick - 1000 - i * 2);
+                    move_to(types[i], mid_x + - v_.getX() , 0, tick - 500 - i * 2);
             }
             return;
-        }
+        }*/
         
         
         if (tick >= 1500 && tick < 1510) {
-            double mid_y = positions[2][1] + 30 * game.getVehicleRadius();            
+            double mid_y = 0;            
             for (int i = 0; i < 5; ++i) {
                 Vehicle v_ =  find_vehicle(me, types[i]);
-                double delta = game.getVehicleRadius();
                 if (Math.abs(v_.getY() - mid_y) > delta)
-                    move_to(types[i], 0, mid_y + 2 * i * delta - v_.getY() , tick - 1500 - i * 2);
+                    move_to(types[i], 0, mid_y - v_.getY() , tick - 1500 - i * 2);
             }
             return;
         }
         
         if (tick >= 2000 && tick < 2002) {
-            double mid_x = positions[2][0] + 30 * game.getVehicleRadius();                    
-            double mid_y = positions[2][1] + 30 * game.getVehicleRadius();
+            double mid_x = 0;                    
+            double mid_y = 0;
                 
             tick -= 2000;
             double factor = 1.0 / 3;
@@ -220,10 +235,10 @@ public final class MyStrategy implements Strategy {
     private Move move;
     
     private VehicleType[] types = {VehicleType.TANK, 
-                                   VehicleType.IFV, 
+                                   VehicleType.IFV,
+                                   VehicleType.ARRV,
                                    VehicleType.HELICOPTER,     
-                                   VehicleType.FIGHTER,
-                                   VehicleType.ARRV};
+                                   VehicleType.FIGHTER};
     
     private void update_vehicle() {
         VehicleUpdate[] vehicle_updates = world.getVehicleUpdates();
@@ -242,32 +257,52 @@ public final class MyStrategy implements Strategy {
     private void init_vehicles() {
         origin_vehicles = world.getNewVehicles();
         vehicles = origin_vehicles;
-        
-        double [] x = new double[5];
-        double [] y = new double[5];
         Vehicle v_;
-        for (int i = 0; i < 5; ++i) {
+        for (int i = 0; i < 3; ++i) {
             v_ = find_vehicle(me, types[i]);
-            positions[i] = new double[] {v_.getX(), v_.getY()};
-            x[i] = positions[i][0];
-            y[i] = positions[i][1];
+            positions0[i] = new Position(v_.getX(), v_.getY(), v_.getType());
         }
         
-        Arrays.sort(x);
-        Arrays.sort(y);
+        for (int i = 3; i < 5; ++i) {
+            v_ = find_vehicle(me, types[i]);
+            positions1[i-3] = new Position(v_.getX(), v_.getY(), v_.getType());
+        }
         
-        Arrays.sort(positions, new java.util.Comparator<double[]>() {
-            public int compare(double[] a, double[] b) {
-                return Double.compare(a[0], b[0]);
+        Arrays.sort(positions0, new java.util.Comparator<Position>() {
+            public int compare(Position a, Position b) {
+                if (a.x != b.x)
+                    return Double.compare(a.x, b.x);
+                return Double.compare(a.y, b.y);
             }
         });
         
-        positions[2] = new double[] {x[2], y[2]};
+        Arrays.sort(positions1, new java.util.Comparator<Position>() {
+            public int compare(Position a, Position b) {
+                if (a.x != b.x)
+                    return Double.compare(a.x, b.x);
+                return Double.compare(a.y, b.y);
+            }
+        });
         
-        for (int i = 0; i < 5; ++i) {
-            System.out.println(Arrays.toString(positions[i]));
-        }
+        System.out.println(Arrays.toString(positions0));
+        System.out.println(Arrays.toString(positions1));
         
+        System.out.println("radius: " + game.getVehicleRadius());
         System.out.println();
     }
+    
+    private Position [] positions0 = new Position[3];
+    private Position [] positions1 = new Position[2];
+}
+
+class Position {
+    public Position(double x, double y, VehicleType type) {
+            this.x = x;
+            this.y = y;
+            this.type = type;
+    }
+    
+    double x;
+    double y;
+    VehicleType type;
 }
